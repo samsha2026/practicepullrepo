c2ed87230122e70887fcac23baf18656eab3b25a
unknown <prasamsha233@gmail.com>
2026-08-25 14:50:18 +0000

    yuhh

--- starter/src/main/java/com/neueda/leap/Main.java
+++ starter/src/main/java/com/neueda/leap/Main.java
@@ -2,6 +2,9 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I'm too quick for him");
+        System.out.println("adsadadadaaaaGTMMM");
+        for(int i = 0; i < 10; i++) {
+            System.out.println("sadasdsadsadada");
+        }
     }
 }
